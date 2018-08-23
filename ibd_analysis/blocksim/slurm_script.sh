5320b174791d4deeda1a89191076ad15a6c76606
Harald Ringbauer <hringbauer@midway2-login2.rcc.local>
2018-08-23 18:06:48 -0500

    Results

--- ibd_analysis/blocksim/slurm_script.sh
+++ ibd_analysis/blocksim/slurm_script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --partition=broadwl
 #SBATCH --ntasks=1
-#SBATCH --job-name="selfing300"
-#SBATCH --time=1:30:00
+#SBATCH --job-name="selfing100reps"
+#SBATCH --time=1:00:00
 #SBATCH --mem=4G
 #SBATCH --mail-user=hringbauer@uchicago.edu
 #SBATCH --mail-type=ALL
 #SBATCH --no-requeue
 #SBATCH --export=NONE
-#SBATCH --array=0-99
+#SBATCH --array=0-199
 unset SLURM_EXPORT_ENV
 
 export OMP_NUM_THREADS=1
